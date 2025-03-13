8772ff01c44739c94f09e9b9e891254d3995b2fd
nqd <nqdinhddt@gmail.com>
2025-03-13 16:27:49 +0800

    update pipeline

--- src/metrics.rs
+++ src/metrics.rs
@@ -31,6 +31,7 @@ struct TransactionMetricsInner {
     pages_split: u32,
 }
 
+// Veryyy long comment Veryyy long comment Veryyy long comment Veryyy long comment Veryyy long comment Veryyy long comment Veryyy long comment Veryyy long comment Veryyy long comment Veryyy long comment
 #[derive(Debug, Default)]
 pub(crate) struct TransactionMetrics {
     inner: RefCell<TransactionMetricsInner>,
