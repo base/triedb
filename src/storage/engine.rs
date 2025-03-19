4467ed18919a4976429f4e2a7d52af26742bf740
Joby Thundil <jjtny1@gmail.com>
2025-03-19 00:50:38 -0400

    add test

--- src/storage/engine.rs
+++ src/storage/engine.rs
@@ -3322,6 +3322,54 @@ mod tests {
         assert_eq!(root_subtrie_contents_before, root_subtrie_contents_after);
     }
 
+    #[test]
+    fn test_leaf_update_and_non_existent_delete_works() {
+        let (storage_engine, mut context) = create_test_engine(300);
+
+        // GIVEN: a trie with a single account
+        let address_nibbles_original_account = Nibbles::unpack(hex!(
+            "0xf80f21938e5248ec70b870ac1103d0dd01b7811550a7a5c971e1c3e85ea62492"
+        ));
+        let account = create_test_account(100, 1);
+        storage_engine
+            .set_values(
+                &mut context,
+                vec![(
+                    AddressPath::new(address_nibbles_original_account.clone()).into(),
+                    Some(account.clone().into()),
+                )]
+                .as_mut(),
+            )
+            .unwrap();
+
+        // WHEN: the same account is modified alongside a delete operation of a non existent value
+        let address_nibbles = Nibbles::unpack(hex!(
+            "0xf80f21938e5248ec70b870ac1103d0dd01b7811550a7ffffffffffffffffffff"
+        ));
+
+        let updated_account = create_test_account(300, 1);
+        storage_engine
+            .set_values(
+                &mut context,
+                vec![
+                    (
+                        AddressPath::new(address_nibbles_original_account.clone()).into(),
+                        Some(updated_account.clone().into()),
+                    ),
+                    (AddressPath::new(address_nibbles).into(), None),
+                ]
+                .as_mut(),
+            )
+            .unwrap();
+
+        // THEN: the updated account should be updated
+        let account_in_database = storage_engine
+            .get_account(&context, AddressPath::new(address_nibbles_original_account).into())
+            .unwrap()
+            .unwrap();
+        assert_eq!(account_in_database, updated_account);
+    }
+
     fn address_path_for_idx(idx: u64) -> AddressPath {
         let mut nibbles = [0u8; 64];
         let mut val = idx;
