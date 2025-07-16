0a1b25405ac798811dcf90e686242dcbfcb7f5a0
William Law <williamlaw.wtl@gmail.com>
2025-07-16 11:40:03 -0400

    only use fields needed for now

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,3 @@
-use std::os::unix::net::SocketAddr;
-
 use crate::page::Page;
 use log::LevelFilter;
 
@@ -9,16 +7,8 @@ use log::LevelFilter;
 pub struct Config {
     /// The maximum number of pages that can be allocated.
     max_pages: u32,
-    /// The maximum number of entries for a single snapshot cache.
-    max_cache_size: usize,
-    /// The limit on total number of concurrent transactions.
-    max_concurrent_transactions: usize,
-    /// The limit on number of threads in the writer's internal thread pool.
-    max_writers: usize,
     /// The log level for the database.
     log_level: LevelFilter,
-    /// The metrics address to export to.
-    metrics_address: Option<SocketAddr>,
 }
 
 impl Config {
@@ -28,28 +18,8 @@ impl Config {
         self
     }
 
-    pub fn with_max_cache_size(mut self, max_cache_size: usize) -> Self {
-        self.max_cache_size = max_cache_size;
-        self
-    }
-
-    pub fn with_max_concurrent_transactions(mut self, max_concurrent_transactions: usize) -> Self {
-        self.max_concurrent_transactions = max_concurrent_transactions;
-        self
-    }
-
-    pub fn with_max_writers(mut self, max_writers: usize) -> Self {
-        self.max_writers = max_writers;
-        self
-    }
-
     pub fn with_log_level(mut self, log_level: LevelFilter) -> Self {
         self.log_level = log_level;
-        self
-    }
-
-    pub fn with_metrics_address(mut self, metrics_address: SocketAddr) -> Self {
-        self.metrics_address = Some(metrics_address);
         self
     }
 
@@ -58,35 +28,13 @@ impl Config {
         self.max_pages
     }
 
-    pub const fn max_concurrent_transactions(&self) -> usize {
-        self.max_concurrent_transactions
-    }
-
-    pub const fn max_writers(&self) -> usize {
-        self.max_writers
-    }
-
     pub const fn log_level(&self) -> LevelFilter {
         self.log_level
-    }
-
-    pub fn metrics_address(&self) -> Option<SocketAddr> {
-        self.metrics_address.clone()
     }
 }
 
 impl Default for Config {
     fn default() -> Self {
-        Self {
-            // Let user outside of database to set this
-            max_pages: Page::MAX_COUNT,
-            max_cache_size: 10,
-            // This would default to an unlimited number (always at most 1 writer)
-            max_concurrent_transactions: usize::MAX,
-            // Currently, we expose at most 1 writer at a given time.
-            max_writers: 1,
-            log_level: LevelFilter::Info,
-            metrics_address: None,
-        }
+        Self { max_pages: Page::MAX_COUNT, log_level: LevelFilter::Info }
     }
 }
