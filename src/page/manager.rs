c50f09af51f8167f5114dec33aeacc913ee23873
nqd <nqdinhddt@gmail.com>
2025-08-04 23:02:33 +0800

    Create page manager interface

--- src/page/manager.rs
+++ src/page/manager.rs
@@ -1,4 +1,9 @@
-use crate::page::PageId;
+use std::io;
+
+use crate::{
+    page::{Page, PageId, PageMut},
+    snapshot::SnapshotId,
+};
 
 pub(super) mod mmap;
 pub(super) mod options;
@@ -19,4 +24,27 @@ pub enum PageError {
     InvalidValue,
     InvalidPageContents(PageId),
     // TODO: add more errors here for other cases.
+}
+
+pub trait PageManagerTrait: Send + Sync + std::fmt::Debug + 'static {
+    /// Retrieves a read-only page from the page manager
+    fn get(&self, snapshot_id: SnapshotId, page_id: PageId) -> Result<Page<'_>, PageError>;
+
+    /// Retrieves a mutable page from the page manager
+    fn get_mut(&self, snapshot_id: SnapshotId, page_id: PageId) -> Result<PageMut<'_>, PageError>;
+
+    /// Allocates a new page
+    fn allocate(&self, snapshot_id: SnapshotId) -> Result<PageMut<'_>, PageError>;
+
+    /// Returns the number of pages currently stored
+    fn size(&self) -> u32;
+
+    /// Returns the maximum number of pages that can be allocated
+    fn capacity(&self) -> u32;
+
+    /// Syncs pages to the backing store
+    fn sync(&self) -> io::Result<()>;
+
+    /// Syncs and closes the page manager
+    fn close(self: Box<Self>) -> io::Result<()>;
 }
--- src/storage/engine.rs
+++ src/storage/engine.rs
@@ -9,8 +9,8 @@ use crate::{
         TrieValue,
     },
     page::{
-        Page, PageError, PageId, PageManager, PageMut, SlottedPage, SlottedPageMut,
-        CELL_POINTER_SIZE,
+        Page, PageError, PageId, PageManagerTrait, PageMut, SlottedPage,
+        SlottedPageMut, CELL_POINTER_SIZE,
     },
     path::{AddressPath, StoragePath, ADDRESS_PATH_LENGTH, STORAGE_PATH_LENGTH},
     pointer::Pointer,
@@ -34,7 +34,7 @@ use std::{
 /// which could be memory-mapped files, in-memory storage, or other implementations.
 #[derive(Debug)]
 pub struct StorageEngine {
-    pub(crate) page_manager: PageManager,
+    pub(crate) page_manager: Box<dyn PageManagerTrait + Send + Sync>,
     pub(crate) meta_manager: Mutex<MetadataManager>,
     pub(crate) alive_snapshot: AtomicU64,
 }
@@ -47,10 +47,10 @@ enum PointerChange {
 }
 
 impl StorageEngine {
-    pub fn new(page_manager: PageManager, meta_manager: MetadataManager) -> Self {
+    pub fn new(page_manager: impl PageManagerTrait, meta_manager: MetadataManager) -> Self {
         let alive_snapshot = meta_manager.active_slot().snapshot_id();
         Self {
-            page_manager,
+            page_manager: Box::new(page_manager),
             meta_manager: Mutex::new(meta_manager),
             alive_snapshot: AtomicU64::new(alive_snapshot),
         }
