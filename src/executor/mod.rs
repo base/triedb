666bf241a14df346ad0f6d00c7acc038b97bdbcb
and-cb <203359594+and-cb@users.noreply.github.com>
2025-08-07 13:43:15 -0700

    Add a new `executor` module to run functions in thread pools (#158)

---
+++ src/executor/mod.rs
@@ -0,0 +1,33 @@
+//! Concurrent execution.
+//!
+//! This module provides structures and traits to run functions in separate threads and obtain
+//! their results later on. The implementation is currently based on the popular [`rayon`] crate.
+//! This module also provides a dummy implementation called [`Inline`] that does not spawn any
+//! actual thread but executes everything serially.
+//!
+//! # Examples
+//!
+//! ```
+//! use triedb::executor::{threadpool, Executor, Wait};
+//!
+//! // Create a thread pool
+//! let pool = threadpool::builder().build().unwrap();
+//!
+//! // Run some closures in the background.
+//! let future1 = pool.defer(|| 1 + 1);
+//! let future2 = pool.defer(|| 2 + 2);
+//!
+//! // Wait for the closures to return a result.
+//! assert_eq!(future1.wait(), &2);
+//! assert_eq!(future2.wait(), &4);
+//! ```
+
+mod futures;
+mod inline;
+mod traits;
+
+pub mod threadpool;
+
+pub use futures::{Future, PoisonError};
+pub use inline::Inline;
+pub use traits::{Executor, Wait};
