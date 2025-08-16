12f227c942fad0fc7bf4b99e5b4495a20b9ab1c4
Brian Bland <brian.bland@coinbase.com>
2025-08-15 17:25:59 -0700

    Minor cleanup

--- src/transaction/error.rs
+++ src/transaction/error.rs
@@ -1,21 +1,11 @@
 use std::{error::Error, fmt};
 
 #[derive(Debug)]
-pub enum TransactionError {
-    /// Generic transaction error (for backward compatibility)
-    Generic,
-    /// Overlay functionality is not enabled for this transaction
-    OverlayNotEnabled,
-}
+pub struct TransactionError;
 
 impl fmt::Display for TransactionError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Generic => write!(f, "transaction error"),
-            Self::OverlayNotEnabled => {
-                write!(f, "overlay functionality is not enabled for this transaction")
-            }
-        }
+        write!(f, "transaction error")
     }
 }
 
